22a837aa6aca1fc0fe7bfd117387159899aa3585
gromaistvan <wavezone@mrginfo.com>
2020-02-04 22:49:25 +0100

    Better Docker

--- MrgInfo.AdoQuery.Sql/database.sh
+++ MrgInfo.AdoQuery.Sql/database.sh
@@ -6,9 +6,12 @@ LOG=$DBA_DIR/database.log
 
 if [ ! -f $LOG ]; then
     echo "Waiting for SQL Server..." >$LOG
-    sleep 2m
+    sleep 1m
     for f in $(find $DBA_DIR -name *.sql); do
         echo "Running script $f." >>$LOG
-        $TOOL_DIR/sqlcmd -S localhost -U SA -P $SA_PASSWORD -d master -l 300 -i $f 2>&1 >>$LOG
+        $TOOL_DIR/sqlcmd -S localhost -U SA -P $SA_PASSWORD -d master -l 60 -i $f 2>&1 >>$LOG
     done
+    cat $LOG
 fi
+
+sleep infinity
