447fab665d82e487e405d983fa1331ee14a29162
gromaistvan <wavezone@mrginfo.com>
2020-02-04 23:13:48 +0100

    Better Docker

--- MrgInfo.AdoQuery.Sql/database.sh
+++ MrgInfo.AdoQuery.Sql/database.sh
@@ -4,14 +4,17 @@ TOOL_DIR=/opt/mssql-tools/bin
 DBA_DIR=/dba
 LOG=$DBA_DIR/database.log
 
+/opt/mssql/bin/sqlservr &
+PID=$!
+
 if [ ! -f $LOG ]; then
     echo "Waiting for SQL Server..." >$LOG
     sleep 1m
-    for f in $(find $DBA_DIR -name *.sql); do
-        echo "Running script $f." >>$LOG
-        $TOOL_DIR/sqlcmd -S localhost -U SA -P $SA_PASSWORD -d master -l 60 -i $f 2>&1 >>$LOG
+    for file in $(find $DBA_DIR -name *.sql); do
+        echo "Running script $file." >>$LOG
+        $TOOL_DIR/sqlcmd -S localhost -U SA -P $SA_PASSWORD -d master -l 120 -i $file 2>&1 >>$LOG
     done
     cat $LOG
 fi
 
-sleep infinity
+wait $PID
